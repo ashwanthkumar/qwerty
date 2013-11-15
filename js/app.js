2ad7ddf7e1a8e6d442db479c3c7af4e7242bbe83
Bharath ツ <bharathraja@live.com>
2013-11-15 07:20:15 +0530

    sending time along with date

--- js/app.js
+++ js/app.js
@@ -81,7 +81,7 @@ $(function(){
             qwerty.xhr2 = $.ajax({//load data for next page
               url: './bus/travel',
               type: 'POST',
-              data: {user_id: qwerty.user_id, from: qwerty.from, to: qwerty.to, date: qwerty.date, bus_id: qwerty.bus_id},
+              data: {user_id: qwerty.user_id, from: qwerty.from, to: qwerty.to, date: qwerty.date, bus_id: qwerty.bus_id, time: $(this).text()+':00'},
               success: function(data){
                 var travel_id = data.travel_id;
                 data = data.data;
